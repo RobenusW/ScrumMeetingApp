a4266b7ea56dc9039950e9ab3f4ddbd298a606f3
RobenusW <robenuswork@gmail.com>
2025-03-02 14:43:28 -0500

    Initial Commit

---
+++ YourRights/YourRightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourRightsApp.swift
+//  YourRights
+//
+//  Created by Robenus Waktole on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct YourRightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YourRightsTests/YourRightsTests.swift
@@ -0,0 +1,17 @@
+//
+//  YourRightsTests.swift
+//  YourRightsTests
+//
+//  Created by Robenus Waktole on 3/2/25.
+//
+
+import Testing
+@testable import YourRights
+
+struct YourRightsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
