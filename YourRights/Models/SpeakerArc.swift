e5822a888a8f4ab63c6cda6628fc849d5fccc56c
RobenusW <robenuswork@gmail.com>
2025-03-09 16:35:39 -0400

    Finished App: Added interactive circle view and added Transcription

---
+++ YourRights/Models/SpeakerArc.swift
@@ -0,0 +1,18 @@
+//
+//  SpeakerArc.swift
+//  YourRights
+//
+//  Created by Robenus Waktole on 3/8/25.
+//
+
+import SwiftUI
+
+struct SpeakerArc: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeakerArc()
+}
